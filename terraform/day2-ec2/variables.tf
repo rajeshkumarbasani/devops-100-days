536f8a6f1ed51fb91f4eb1bbdb71f8e557b052c6
rajeshkumarbasani <raj358822@gmail.com>
2026-05-29 13:28:36 +0530

    Updated VPC, Subnet and IG

--- terraform/day2-ec2/variables.tf
+++ terraform/day2-ec2/variables.tf
@@ -7,6 +7,7 @@ variable "ami_id"{
     default = "ami-0f58b397bc5c1f2e8"
     description = "AMI ID for EC2 instance"
 }
+variable "aws_availability_zone" {}
 
 variable "instance_type"{
     default = "t2.micro"
