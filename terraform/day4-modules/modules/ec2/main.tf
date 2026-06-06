507dd69b93eb63f71d50246b0a2a964d81fb3655
rajeshkumarbasani <raj358822@gmail.com>
2026-06-06 12:41:21 +0530

    added my public ip

--- terraform/day4-modules/modules/ec2/main.tf
+++ terraform/day4-modules/modules/ec2/main.tf
@@ -42,7 +42,7 @@ resource "aws_instance" "this" {
     ami           = var.ami_id
     instance_type = var.instance_type
     subnet_id     = var.subnet_id
-    vpc_security_groups_ids  = [aws_security_group.ec2_sg.id]
+    vpc_security_group_ids  = [aws_security_group.ec2_sg.id]
     key_name      = var.key_name
     associate_public_ip_address = true
 
