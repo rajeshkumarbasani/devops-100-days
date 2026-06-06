850797a655031d832960e891bb7315739d6697e1
rajeshkumarbasani <raj358822@gmail.com>
2026-06-06 07:44:37 +0000

    Pushing final code

--- terraform/day4-modules/modules/ec2/main.tf
+++ terraform/day4-modules/modules/ec2/main.tf
@@ -1,52 +1,53 @@
 resource "aws_security_group" "ec2_sg" {
-    name = "${var.instance_name}-sg"
-    description = "Security Group for EC2"
+  name        = "${var.instance_name}-sg"
+  description = "Security Group for EC2"
+  vpc_id      = var.vpc_id
 
-    ingress {
-        description = "Allow SSH"
-        from_port   = 22
-        to_port     = 22
-        protocol    = "tcp"
-        cidr_blocks = [var.my_ip/32]
-    }
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
 
-    ingress {
-        description = "Allow HTTP"
-        from_port   = 80
-        to_port     = 80
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    ingress {
-        description = "Allow Jenkins"
-        from_port   = 8080
-        to_port     = 8080
-        protocol    = "tcp"
-        cidr_blocks = [var.my_ip/32]
-    }
+  ingress {
+    description = "Allow Jenkins"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
 
-    egress {
-        description = "Allow all outbound traffic"
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    tags = {
-        Name = "${var.instance_name}-sg"
-    }
+  tags = {
+    Name = "${var.instance_name}-sg"
+  }
 }
 resource "aws_instance" "this" {
-    ami           = var.ami_id
-    instance_type = var.instance_type
-    subnet_id     = var.subnet_id
-    vpc_security_group_ids  = [aws_security_group.ec2_sg.id]
-    key_name      = var.key_name
-    associate_public_ip_address = true
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  subnet_id                   = var.subnet_id
+  vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
+  key_name                    = var.key_name
+  associate_public_ip_address = true
 
-    tags = {
-        Name = var.instance_name
-    }
+  tags = {
+    Name = var.instance_name
+  }
 }
--- terraform/day4-modules/modules/ec2/outputs.tf
+++ terraform/day4-modules/modules/ec2/outputs.tf
@@ -3,9 +3,9 @@ output "instance_id" {
 }
 
 output "public_ip" {
-    value = aws_instance.this.public_ip
+  value = aws_instance.this.public_ip
 }
 
 output "security_group_id" {
-    value = aws_security_group.ec2_sg.id
+  value = aws_security_group.ec2_sg.id
 }
