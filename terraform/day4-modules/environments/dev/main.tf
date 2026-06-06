87cc9f9a147ceb3c5135dbd303bf643642290cc3
rajeshkumarbasani <raj358822@gmail.com>
2026-06-06 07:03:51 +0000

    fixed cider block

--- terraform/day4-modules/environments/dev/main.tf
+++ terraform/day4-modules/environments/dev/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_vpc" "day4_vpc" {
-  cidr_block           = "var.vpc_cidr"
+  cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
 
