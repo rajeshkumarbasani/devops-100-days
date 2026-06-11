3457c947fa0d4b2d12eceda4b14ba76abc1a181e
rajeshkumarbasani <raj358822@gmail.com>
2026-06-11 21:43:51 +0530

    Updated terraform fmt code

--- terraform/day6/modules/ec2/main.tf
+++ terraform/day6/modules/ec2/main.tf
@@ -1,38 +1,38 @@
 resource "aws_security_group" "this" {
-    name ="${var.project_name}-${var.environment}-sg"
-    description = "Day 6 EC2 security group"
+  name        = "${var.project_name}-${var.environment}-sg"
+  description = "Day 6 EC2 security group"
 
-    vpc_id = var.vpc_id
+  vpc_id = var.vpc_id
 
-    ingress {
-        description = "SSH from my IP"
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = [var.my_ip]
+  ingress {
+    description = "SSH from my IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip]
 
-    }
+  }
 
-    ingress {
-        description = "HTTP from internet"
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = var.http_allowed_cidr
+  ingress {
+    description = "HTTP from internet"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = var.http_allowed_cidr
 
-    }
-    egress {
-        description = "Allow all outbound traffic"
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  }
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    tags = merge(var.tags, {
-        Name = "${var.project_name}-${var.environment}-sg"
+  tags = merge(var.tags, {
+    Name = "${var.project_name}-${var.environment}-sg"
 
-    })
+  })
 }
 
 
