5bae779d32c21f9bbc62fbf38f64949ea8ba3848
rajeshkumarbasani <raj358822@gmail.com>
2026-06-11 22:12:14 +0530

    Updated terraform fmt code

--- terraform/day6/env/dev/main.tf
+++ terraform/day6/env/dev/main.tf
@@ -28,11 +28,11 @@ module "ec2" {
   instance_type = var.instance_type
   key_name      = var.key_name
 
-  vpc_id    = module.vpc.vpc_id
-  subnet_id = module.vpc.public_subnet_id
-  my_ip     = var.my_ip
-  volume_type           = var.volume_type
-  volume_size           = var.volume_size
+  vpc_id      = module.vpc.vpc_id
+  subnet_id   = module.vpc.public_subnet_id
+  my_ip       = var.my_ip
+  volume_type = var.volume_type
+  volume_size = var.volume_size
 
 
 }
--- terraform/day6/env/dev/variables.tf
+++ terraform/day6/env/dev/variables.tf
@@ -8,19 +8,19 @@ variable "key_name" {}
 variable "my_ip" {}
 
 variable "vpc_cidr" {
-    default = "10.60.0.0/16"
+  default = "10.60.0.0/16"
 }
 
 variable "public_subnet_cidr" {
   default = "10.60.1.0/24"
 }
 
-variable "volume_size"{
-    default = 20
+variable "volume_size" {
+  default = 20
 }
 
-variable "volume_type"{
-    default = "gp3"
+variable "volume_type" {
+  default = "gp3"
 }
 
 
